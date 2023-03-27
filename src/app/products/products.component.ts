1a25a258631d5879295b73e41b11d30bf2762cd8
olusoladeboy <goldblumie1@gmail.com>
2023-03-27 16:04:31 +0100

    Product Page

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -7,97 +7,159 @@ import { Router } from '@angular/router';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent {
-  productList: { [key: string]: string }[] = [
-    {
-      id: '1',
-      title: 'Essential Men’s Short-Sleeve Crewneck T-Shirt',
-      category: 'Shirt',
-      rating: '4.9',
-      salesCount: '2356',
-      price: '12.00',
-      imageUrl: 'assets/images/clothing-1.png'
-    },
-    {
-      id: '2',
-      title: 'Essential Men’s Short-Sleeve Crewneck T-Shirt',
-      category: 'Shirt',
-      rating: '4.9',
-      salesCount: '2356',
-      price: '12.00',
-      imageUrl: 'assets/images/clothing-1.png'
-    },
-    {
-      id: '1',
-      title: 'Essential Men’s Short-Sleeve Crewneck T-Shirt',
-      category: 'Shirt',
-      rating: '4.9',
-      salesCount: '2356',
-      price: '12.00',
-      imageUrl: 'assets/images/clothing-1.png'
-    }, {
-      id: '1',
-      title: 'Essential Men’s Short-Sleeve Crewneck T-Shirt',
-      category: 'Shirt',
-      rating: '4.9',
-      salesCount: '2356',
-      price: '12.00',
-      imageUrl: 'assets/images/clothing-1.png'
-    },
-    {
-      id: '2',
-      title: 'Essential Men’s Short-Sleeve Crewneck T-Shirt',
-      category: 'Shirt',
-      rating: '4.9',
-      salesCount: '2356',
-      price: '12.00',
-      imageUrl: 'assets/images/clothing-1.png'
-    },
-    {
-      id: '2',
-      title: 'Essential Men’s Short-Sleeve Crewneck T-Shirt',
-      category: 'Shirt',
-      rating: '4.9',
-      salesCount: '2356',
-      price: '12.00',
-      imageUrl: 'assets/images/clothing-1.png'
-    },
-    {
-      id: '2',
-      title: 'Essential Men’s Short-Sleeve Crewneck T-Shirt',
-      category: 'Shirt',
-      rating: '4.9',
-      salesCount: '2356',
-      price: '12.00',
-      imageUrl: 'assets/images/clothing-1.png'
-    },
-    {
-      id: '2',
-      title: 'Essential Men’s Short-Sleeve Crewneck T-Shirt',
-      category: 'Shirt',
-      rating: '4.9',
-      salesCount: '2356',
-      price: '12.00',
-      imageUrl: 'assets/images/clothing-1.png'
-    },
-    {
-      id: '2',
-      title: 'Essential Men’s Short-Sleeve Crewneck T-Shirt',
-      category: 'Shirt',
-      rating: '4.9',
-      salesCount: '2356',
-      price: '12.00',
-      imageUrl: 'assets/images/clothing-1.png'
-    },
-    {
-      id: '2',
-      title: 'Essential Men’s Short-Sleeve Crewneck T-Shirt',
-      category: 'Shirt',
-      rating: '4.9',
-      salesCount: '2356',
-      price: '12.00',
-      imageUrl: 'assets/images/clothing-1.png'
-    },
-  ]
+  productList: {
+    [key: string]: any,
+  }[] = [
+      {
+        id: '1',
+        title: 'Essential Men’s Short-Sleeve Crewneck T-Shirt',
+        category: 'Shirt',
+        rating: '4.9',
+        salesCount: '2356',
+        price: '12.00',
+        imageUrl: 'assets/images/clothing-1.png',
+        images: [
+          'assets/images/clothing-1.png',
+          'assets/images/clothing-1.png',
+          'assets/images/clothing-1.png',
+          'assets/images/clothing-1.png',
+        ]
+      },
+      {
+        id: '2',
+        title: 'Essential Men’s Short-Sleeve Crewneck T-Shirt',
+        category: 'Shirt',
+        rating: '4.9',
+        salesCount: '2356',
+        price: '12.00',
+        imageUrl: 'assets/images/clothing-2.png',
+        images: [
+          'assets/images/clothing-1.png',
+          'assets/images/clothing-1.png',
+          'assets/images/clothing-1.png',
+          'assets/images/clothing-1.png',
+        ]
+      },
+      {
+        id: '1',
+        title: 'Essential Men’s Short-Sleeve Crewneck T-Shirt',
+        category: 'Shirt',
+        rating: '4.9',
+        salesCount: '2356',
+        price: '12.00',
+        imageUrl: 'assets/images/clothing-1.png',
+        images: [
+          'assets/images/clothing-1.png',
+          'assets/images/clothing-1.png',
+          'assets/images/clothing-1.png',
+          'assets/images/clothing-1.png',
+        ]
+      }, {
+        id: '1',
+        title: 'Essential Men’s Short-Sleeve Crewneck T-Shirt',
+        category: 'Shirt',
+        rating: '4.9',
+        salesCount: '2356',
+        price: '12.00',
+        imageUrl: 'assets/images/clothing-2.png',
+        images: [
+          'assets/images/clothing-1.png',
+          'assets/images/clothing-1.png',
+          'assets/images/clothing-1.png',
+          'assets/images/clothing-1.png',
+        ]
+      },
+      {
+        id: '2',
+        title: 'Essential Men’s Short-Sleeve Crewneck T-Shirt',
+        category: 'Shirt',
+        rating: '4.9',
+        salesCount: '2356',
+        price: '12.00',
+        imageUrl: 'assets/images/clothing-1.png',
+        images: [
+          'assets/images/clothing-1.png',
+          'assets/images/clothing-1.png',
+          'assets/images/clothing-1.png',
+          'assets/images/clothing-1.png',
+        ]
+      },
+      {
+        id: '2',
+        title: 'Essential Men’s Short-Sleeve Crewneck T-Shirt',
+        category: 'Shirt',
+        rating: '4.9',
+        salesCount: '2356',
+        price: '12.00',
+        imageUrl: 'assets/images/clothing-1.png',
+        images: [
+          'assets/images/clothing-1.png',
+          'assets/images/clothing-1.png',
+          'assets/images/clothing-1.png',
+          'assets/images/clothing-1.png',
+        ]
+      },
+      {
+        id: '2',
+        title: 'Essential Men’s Short-Sleeve Crewneck T-Shirt',
+        category: 'Shirt',
+        rating: '4.9',
+        salesCount: '2356',
+        price: '12.00',
+        imageUrl: 'assets/images/clothing-1.png',
+        images: [
+          'assets/images/clothing-1.png',
+          'assets/images/clothing-1.png',
+          'assets/images/clothing-1.png',
+          'assets/images/clothing-1.png',
+        ]
+      },
+      {
+        id: '2',
+        title: 'Essential Men’s Short-Sleeve Crewneck T-Shirt',
+        category: 'Shirt',
+        rating: '4.9',
+        salesCount: '2356',
+        price: '12.00',
+        imageUrl: 'assets/images/clothing-1.png',
+        images: [
+          'assets/images/clothing-1.png',
+          'assets/images/clothing-1.png',
+          'assets/images/clothing-1.png',
+          'assets/images/clothing-1.png',
+        ]
+      },
+      {
+        id: '2',
+        title: 'Essential Men’s Short-Sleeve Crewneck T-Shirt',
+        category: 'Shirt',
+        rating: '4.9',
+        salesCount: '2356',
+        price: '12.00',
+        imageUrl: 'assets/images/clothing-1.png',
+        images: [
+          'assets/images/clothing-1.png',
+          'assets/images/clothing-1.png',
+          'assets/images/clothing-1.png',
+          'assets/images/clothing-1.png',
+        ]
+      },
+      {
+        id: '2',
+        title: 'Essential Men’s Short-Sleeve Crewneck T-Shirt',
+        category: 'Shirt',
+        rating: '4.9',
+        salesCount: '2356',
+        price: '12.00',
+        imageUrl: 'assets/images/clothing-1.png',
+        images: [
+          'assets/images/clothing-1.png',
+          'assets/images/clothing-1.png',
+          'assets/images/clothing-1.png',
+          'assets/images/clothing-1.png',
+        ]
+      },
+    ]
 
   constructor(private router: Router) {
 
