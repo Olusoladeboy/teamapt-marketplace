874b158d324607e10a1080c406e948ddc4a85fcb
olusoladeboy <goldblumie1@gmail.com>
2023-03-27 11:43:54 +0100

    Item Page

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent {
-
+  productList: { [key: string]: string }[] = [
+    {
+      id: '1',
+      title: 'Essential Men’s Short-Sleeve Crewneck T-Shirt',
+      category: 'Shirt',
+      rating: '4.9',
+      salesCount: '2356',
+      price: '12.00',
+      imageUrl: 'assets/images/clothing-1.png'
+    },
+    {
+      id: '2',
+      title: 'Essential Men’s Short-Sleeve Crewneck T-Shirt',
+      category: 'Shirt',
+      rating: '4.9',
+      salesCount: '2356',
+      price: '12.00',
+      imageUrl: 'assets/images/clothing-1.png'
+    },
+    {
+      id: '1',
+      title: 'Essential Men’s Short-Sleeve Crewneck T-Shirt',
+      category: 'Shirt',
+      rating: '4.9',
+      salesCount: '2356',
+      price: '12.00',
+      imageUrl: 'assets/images/clothing-1.png'
+    }, {
+      id: '1',
+      title: 'Essential Men’s Short-Sleeve Crewneck T-Shirt',
+      category: 'Shirt',
+      rating: '4.9',
+      salesCount: '2356',
+      price: '12.00',
+      imageUrl: 'assets/images/clothing-1.png'
+    },
+  ]
 }
